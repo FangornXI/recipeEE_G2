82a3973d2d4c84efd6cfb05c2bb0c32445567f75
unknown <quentin.fosse44@gmail.com>
2022-10-05 16:37:57 +0200

    Ajout cuisinee aujourd'hui

--- src/main/java/com/example/recipeee_g2/servlet/RecipeDetailServlet.java
+++ src/main/java/com/example/recipeee_g2/servlet/RecipeDetailServlet.java
@@ -1,6 +1,7 @@
 package com.example.recipeee_g2.servlet;
 
 import com.example.recipeee_g2.dao.DaoFactory;
+import com.example.recipeee_g2.entity.CookedRecipeEntity;
 import com.example.recipeee_g2.entity.RecipeEntity;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 
 @WebServlet("/recipe")
@@ -25,7 +27,7 @@ public class RecipeDetailServlet extends HttpServlet {
 
             req.setAttribute("recipe",foundRecipe.get());
             req.setAttribute("recipeIngredients",foundRecipe.get().getRecipeIngredientsById());
-            req.setAttribute("comments",foundRecipe.get().getCommentsById());
+//            req.setAttribute("comments",foundRecipe.get().getCommentsById());
             req.setAttribute("steps",foundRecipe.get().getStepsById());
             //req.setAttribute("cookedRecipes",foundRecipe.get().getCookedRecipesById());
 
@@ -33,6 +35,17 @@ public class RecipeDetailServlet extends HttpServlet {
         }else {
             resp.sendRedirect(req.getContextPath() + "/homepage");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int idRecipe= Integer.parseInt(req.getParameter("id"));
+        int iduser = Integer.parseInt(req.getSession().getAttribute("user").toString());
+        java.util.Date now = new java.util.Date();
+        java.sql.Date NOW = new java.sql.Date(now.getTime());
+        DaoFactory.getCookedRecipeDAO().create(new CookedRecipeEntity(NOW, DaoFactory.getUserDAO().findById(iduser).get(),DaoFactory.getRecipeDAO().findById(idRecipe).get()));
+
+        resp.sendRedirect(req.getContextPath() + "/homepage");
 
     }
 }
